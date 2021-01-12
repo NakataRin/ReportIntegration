85b659aa083a5ae1081657ceab29b089730aa080
NakataRin <45946146+NakataRin@users.noreply.github.com>
2021-01-12 16:41:19 +0400

    1.1.1

---
+++ Handlers/Server.cs
@@ -0,0 +1,53 @@
+﻿namespace ReportIntegration.Handlers
+{
+    using Exiled.API.Features;
+    using Exiled.Events.EventArgs;
+    using System.Collections.Specialized;
+
+    internal sealed class Server
+    {
+        public static void sendWebHook(string URL, string msg)
+        {
+            Http.Post(URL, new NameValueCollection()
+            {
+                { "content", msg }
+            });
+        }
+        
+        public void OnReport(ReportingCheaterEventArgs ev)
+        {
+            string type = ReportIntegration.Instance.Config.Type;
+            string url = ReportIntegration.Instance.Config.WebHookUrl;
+            bool sendToDevs = ReportIntegration.Instance.Config.SendToNorthwood;
+
+            if (type == "local") return;
+
+            if (type != "cheats" && type != "both" && type != "local")
+            {
+                Log.Warn("[ReportIntegration] There's an error in your config. Solution: just delete all settings related to the plugin.");
+                return;
+            }
+
+            if (!sendToDevs) ev.IsAllowed = false;
+            else ev.IsAllowed = true;
+
+            sendWebHook(url, $"{ev.Reporter.UserId} ({ev.Reporter.Nickname}) has send a report to {ev.Reported.UserId} ({ev.Reported.Nickname}): ''{ev.Reason}''");
+        }
+
+        public void OnLocalReport(LocalReportingEventArgs ev)
+        {
+            string type = ReportIntegration.Instance.Config.Type;
+            string url = ReportIntegration.Instance.Config.WebHookUrl;
+
+            if (type == "cheats") return;
+
+            if (type != "cheats" && type != "both" && type != "local")
+            {
+                Log.Warn("[ReportIntegration] There's an error in your config. Solution: just delete all settings related to the plugin.");
+                return;
+            }
+
+            sendWebHook(url, $"{ev.Issuer.UserId} ({ev.Issuer.Nickname}) has send a local report to {ev.Target.UserId} ({ev.Target.Nickname}): ''{ev.Reason}''");
+        }
+    }
+}
---
+++ ReportIntegration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+
+using Server = Exiled.Events.Handlers.Server;
+
+namespace ReportIntegration
+{
+    public class ReportIntegration : Plugin<Config>
+    {
+        private static readonly Lazy<ReportIntegration> LazyInstance = new Lazy<ReportIntegration>(() => new ReportIntegration());
+        public static ReportIntegration Instance => LazyInstance.Value;
+
+        public override PluginPriority Priority { get; } = PluginPriority.Medium;
+
+        private Handlers.Server server;
+
+        private ReportIntegration()
+        {
+
+        }
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+
+            RegisterEvents();
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+
+            UnregisterEvents();
+        }
+
+        public void RegisterEvents()
+        {
+            server = new Handlers.Server();
+
+            Server.ReportingCheater += server.OnReport;
+            Server.LocalReporting += server.OnLocalReport;
+        }
+
+        public void UnregisterEvents()
+        {
+            Server.ReportingCheater -= server.OnReport;
+            Server.LocalReporting -= server.OnLocalReport;
+
+            server = null;
+        }
+    }
+}
